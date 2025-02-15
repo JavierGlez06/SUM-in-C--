c1e5aa5b375515504af73071466a82f301814007
JavierGlez06 <l20140191@queretaro.tecnm.mx>
2025-02-15 12:17:44 -0600

    crear una suma, en modo facil para un principiante

---
+++ holamundo.cpp
@@ -0,0 +1,11 @@
+// Programa holamundo.cpp
+// el programa visualiza 'Hola Mundo"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hola Mundo!" << endl;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,23 @@
+// Suma de dos numeros sencillos 
+// Autor: Emilio Juarez
+// github: JavierGlez06
+// instagram: emiliojjg
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b , c;
+
+    cout << "Introduce el primer número: ";
+    cin >> a;
+
+    cout << "Introduce el segundo número: ";
+    cin >> b;
+    c = a + b;
+
+    cout << "La suma es de: "<<c << endl;
+    return 0;
+}
